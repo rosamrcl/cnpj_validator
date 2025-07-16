31b022dd280bc1271cbe99cf5b7bacf7b03f9b8e
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-16 15:37:13 -0300

    .

---
+++ CNPJValidator.php
@@ -0,0 +1,5 @@
+<?php
+namespace CNPJ_Validator;
+class CNPJValidator
+
+?>
---
+++ CNPJValidatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CNPJ_Validator;
+
+use PHPUnit\Framework\TestCase;
+use CNPJ_Validator;
+
+class CNPJValidatorTest extends TestCase{
+    
+}
+
+?>
