049eea8fa15740e5a512cf1f220ad2101268aab9
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-16 17:24:53 -0300

    .

--- CNPJValidator.php
+++ CNPJValidator.php
@@ -1,5 +1,43 @@
 <?php
-namespace CNPJ_Validator;
-class CNPJValidator
+class CNPJValidator{
+    public function isValid(string $cnpj):bool{
+        $cnpj = preg_replace('/[^0-9]/', '', $cnpj);
+
+        if(strlen($cnpj) !=14){
+            return false;
+        }
+
+        if (preg_match('/^(\d)\1{13}$/', $cnpj)) {
+            return false;
+        }
+
+        $firstDigit = $firstDigit = $this->calculateDigit(substr($cnpj, 0, 12), [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]);
+        if($cnpj[12] != $firstDigit){
+            return false;
+        }
+        $secondDigit = $this->calculateDigit(substr($cnpj, 0, 13), [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]);
+        if($cnpj[13] != $secondDigit){
+            return false;
+        }
+        return true;
+        
+    }
+
+    private function calculateDigit(string $base, array $multipliers): string{
+        $sum = 0;
+
+        for ($i=0; $i <strlen($base); $i++){
+            $sum+= $base[$i] * $multipliers[$i];
+        }
+
+        $remainder = $sum % 11;
+
+        if ($remainder <2){
+            return '0';
+        }else{
+            return (string)(11-$remainder);
+        }
+    }
+}
 
 ?>
--- CNPJValidatorTest.php
+++ CNPJValidatorTest.php
@@ -1,12 +1,36 @@
 <?php
 
-namespace CNPJ_Validator;
-
 use PHPUnit\Framework\TestCase;
-use CNPJ_Validator;
+use PHPUnit\Framework\Attributes\DataProvider;
 
-class CNPJValidatorTest extends TestCase{
-    
+require_once 'CNPJValidator.php';
+
+class CNPJValidatorTest extends TestCase
+{
+    /**
+     * @dataProvider cnpjProvider
+     */
+
+    public function testIsValid($cnpj, $expected)
+    {
+        $validator = new CNPJValidator();
+        $this->assertEquals($expected, $validator->isValid($cnpj));
+    }
+
+    public function cnpjProvider()
+    {
+        return [
+            ["11.222.333/0001-81", true],
+            ["11222333000181", true],
+            ["00.000.000/0000-00", false],
+            ["12345678901234", false],
+            ["11.111.111/1111-11", false],
+            ["67.654.321/0001-55", false],
+            ["06.990.590/0001-23", true],
+            ["06990590000123", true],
+            ["", false],
+            ["abc", false],
+            ["123", false]
+        ];
+    }
 }
-
-?>
